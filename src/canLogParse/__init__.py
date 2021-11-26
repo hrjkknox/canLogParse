86f6849c9e33bdd0613a566efad7586cbe4e0163
Hamish Knox <hrjkknox@gmail.com>
2021-11-26 16:31:28 +0000

    Create basic utility functions

---
+++ src/canLogParse/__init__.py
@@ -0,0 +1,54 @@
+def fileToList(file):
+	with open(file) as readFile:
+		lines = readFile.readlines()
+	return lines
+
+# Takes a part-formed packet array and returns the data
+def _extractDataFromPacket(packet):
+	dataLength = int(packet[2])
+	# Remove all non-data bytes
+	# I know this is janky, but I'm doing this quickly
+	packet.pop(0)
+	packet.pop(0)
+	packet.pop(0)
+	packet.pop(-1)
+	packet.pop(-1)
+	
+	# Convert the array of strings to an array of bytes
+	dataArray = []
+	for i in packet:
+		dataArray.append(bytes.fromhex(i))
+
+	return dataArray
+
+# Turns the packet into a nice dictionary
+def formatPacketDict(leadingZero, id, dataLength, data, tr, timeStamp):
+	outputDict = {}
+	outputDict["leadingZero"] = leadingZero
+	outputDict["id"] = id
+	outputDict["dataLength"] = dataLength
+	outputDict["data"] = data
+	outputDict["T/R"] = tr
+	outputDict["timeStamp"] = timeStamp
+
+	return outputDict
+
+# Turns the data into a nice list
+def formatPacketList(leadingZero, id, dataLength, data, tr, timeStamp):
+	outputArr = []
+	outputArr.append(leadingZero)
+	outputArr.append(id)
+	outputArr.append(dataLength)
+	outputArr.append(data)
+	outputArr.append(tr)
+	outputArr.append(timeStamp)
+
+	return outputArr
+
+# Turns the data into a nice tuple
+def formatPacketTuple(leadingZero, id, dataLength, data, tr, timeStamp):
+	packetArray = formatPacketList(leadingZero, id, dataLength, data, tr, timeStamp)
+	packetTuple = tuple(packetArray)
+
+	return packetTuple
+
